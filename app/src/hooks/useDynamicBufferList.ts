1003dbeb0bb5186f5100ce421b3d98752e31eaa3
RafaelAlmeida00 <rafaelalmeid00@gmail.com>
2026-01-12 00:03:53 -0300

    a

--- app/src/hooks/useDynamicBufferList.ts
+++ app/src/hooks/useDynamicBufferList.ts
@@ -1,6 +1,12 @@
+
 import { useDynamicRowHeight } from 'react-window';
 
 export function useDynamicBufferList() {
+  if (typeof window === 'undefined') {
+    // SSR: retorna um valor default para evitar erro de ResizeObserver
+    return 140;
+  }
+  // Client: usa o hook normalmente
   return useDynamicRowHeight({
     defaultRowHeight: 140,
   });
